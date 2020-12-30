9247a19626ccf951d5f86be0984dcb5a406f0438
Christophe Bernard <DrakeXorn@users.noreply.github.com>
2020-12-30 16:03:17 +0100

    Changed address mapping to correctly load the establishments on the map, created the establishment reservation fragment, and created access to the API (added unsafe OkHttpClient to allow the data to transit with the https API)

--- app/src/main/java/be/henallux/ig3/smartcity/elbatapp/repositories/web/RetrofitConfigurationService.java
+++ app/src/main/java/be/henallux/ig3/smartcity/elbatapp/repositories/web/RetrofitConfigurationService.java
@@ -5,13 +5,22 @@ import android.content.Context;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory;
 
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import be.henallux.ig3.smartcity.elbatapp.utils.ConnectivityCheckInterceptor;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class RetrofitConfigurationService {
-    private static final String BASE_URL = "http://10.0.2.2:3002/";
+    private static final String BASE_URL = "https://api-elbat.christophebernard.be:3000/";
     private Retrofit retrofitClient;
     private OkHttpClient client;
 
@@ -23,7 +32,7 @@ public class RetrofitConfigurationService {
     }
 
     private RetrofitConfigurationService(Context context) {
-        client = new OkHttpClient.Builder()
+        client = getUnsafeOkHttpClient()
                 .addInterceptor(new ConnectivityCheckInterceptor(context))
                 .build();
 
@@ -36,6 +45,44 @@ public class RetrofitConfigurationService {
                 .addConverterFactory(MoshiConverterFactory.create(moshiConverter))
                 .baseUrl(BASE_URL)
                 .build();
+    }
+
+    public static OkHttpClient.Builder getUnsafeOkHttpClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                            // Ignored to permit the access to the API (non-recognized certificate)
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                            // Ignored to permit the access to the API (non-recognized certificate)
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            OkHttpClient.Builder builder = new OkHttpClient.Builder();
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+            builder.hostnameVerifier((hostname, session) -> true);
+            return builder;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static RetrofitConfigurationService getInstance(Context context) {
--- app/src/main/java/be/henallux/ig3/smartcity/elbatapp/ui/MainActivity.java
+++ app/src/main/java/be/henallux/ig3/smartcity/elbatapp/ui/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.auth0.android.jwt.JWT;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -57,6 +59,11 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
